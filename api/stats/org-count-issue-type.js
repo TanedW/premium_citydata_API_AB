2cc35502d0aa38245a2cfbc55c7066b339eefd0c
Taned_Wonpoo <151303397+TanedW@users.noreply.github.com>
2025-12-15 14:15:05 +0700

    change t.id to issue_id

--- api/stats/org-count-issue-type.js
+++ api/stats/org-count-issue-type.js
@@ -45,12 +45,13 @@ export default async function handler(req) {
       FROM issue_cases i
       JOIN case_organizations co ON i.issue_cases_id = co.case_id
       JOIN org_tree o ON co.organization_id = o.organization_id
-      LEFT JOIN issue_types t ON i.issue_type_id = t.id
+      LEFT JOIN issue_types t ON i.issue_type_id = t.issue_id
       WHERE t.issue_type_name IS NOT NULL
       GROUP BY t.issue_type_name
       ORDER BY count DESC
       LIMIT 10;
     `;
+    
 
     // Map ผลลัพธ์
     const problem_type_stats = problemTypes.map((item, index) => ({
